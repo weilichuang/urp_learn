ab1c34b7e8a41788633ac0bcf756044c1792572d
andy <weilichuang@gmail.com>
2021-06-11 11:31:33 +0800

    LowRes

---
+++ Assets/URP/RendererFeatures/LowResEffect/README.md
@@ -0,0 +1,16 @@
+# LowRes
+Low resolution effect for Unity URP 
+
+![scene](images/scene.png)
+
+Add LowRes RendererFeature to your forward renderer
+
+![scene](images/renderer.png)
+
+Add LowRes effect in post processing volume
+
+![scene](images/pp.png)
+
+Height - screen height in pixels, width depends on aspect ratio
+
+![scene](images/pp setup.png)
---
+++ Assets/URP/RendererFeatures/LowResEffect/Scripts/LowResRenderFeature.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Rendering.Universal;
+
+public class LowResRenderFeature : ScriptableRendererFeature
+{
+    private LowResPass lowResPass;
+
+    public override void Create()
+    {
+        lowResPass = new LowResPass(RenderPassEvent.BeforeRenderingPostProcessing);
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        lowResPass.Setup(renderer.cameraColorTarget);
+        renderer.EnqueuePass(lowResPass);
+    }
+}
